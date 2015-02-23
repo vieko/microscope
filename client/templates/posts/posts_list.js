586f840f80d15cd44afb2e58519b517bd6c2df33
Vieko Franetovic <vieko.franetovic@gmail.com>
2015-02-23 13:42:23 -0700

    adding spinner to router

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: 'Skystone Website',
-    url: 'http://www.skystone.ca/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Another Website',
-    url: 'http://legrudgerugged.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function () {
+    return Posts.find();
+  }
 });
