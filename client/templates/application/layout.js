89c19826d2d1a21e964dbbf2793b9a7e04d096eb
Vieko Franetovic <vieko.franetovic@gmail.com>
2015-02-25 14:28:12 -0700

    added transitions for pages

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,10 +1,15 @@
-// Template.layout.helpers({
-//   pageTitle: function () {
-//     var title = Session.get('pageTitle');
-//     if (title) {
-//       return title;
-//     } else {
-//       return 'Skystone Links';
-//     }
-//   }
-// });
+Template.layout.rendered = function () {
+  this.find('#main')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function (node) {
+      $(node).fadeOut(function () {
+        $(this).remove();
+      });
+    }
+  };
+};
